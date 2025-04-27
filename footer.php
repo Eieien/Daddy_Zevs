303f98936bf11c583f0f4f90d41b96df277fa189
Eien <depedronico02@gmail.com>
2025-04-28 00:09:35 +0800

    first commit

---
+++ footer.php
@@ -0,0 +1,32 @@
+<footer id="footer">
+    <img class="jeb" src="./images/logos/Daddy_zev_no_outline 1.svg" draggable="false">
+    <div class="footer-container">
+        <div class="details">
+            <h1>
+                DADDY ZEV<span>'</span>S
+            </h1>
+            <div class="contact">
+                <img src="./images/socials/mail.svg">daddyzev2025@gmail.com<br>
+            </div>
+            <div class="contact">
+                <img src="./images/socials/phone.svg">+63 0987 656 1532
+            </div>
+        </div>
+        <div class="icon-copyright">
+            <div class="icons">
+                <a href="#">
+                    <img src="./images/socials/Instagram.svg">
+                </a>
+                <a href="#">
+                    <img src="./images/socials/Facebook.svg">
+                </a>
+                <a href="#">
+                    <img src="./images/socials/Tiktok.svg">
+                </a>
+            </div>
+            <div>
+                @ 2025 Daddy Zev’s | All Rights Reserved .  
+            </div>
+        </div>
+    </div>
+</footer>
