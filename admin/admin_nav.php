e642820bf23c9b002efe488dee0e482f0d26253e
Ivan Paul Ruelan <greencolorapple@gmail.com>
2025-05-05 14:06:56 +0800

    updated

--- admin/admin_nav.php
+++ admin/admin_nav.php
@@ -2,9 +2,9 @@
     <div id="nav-container">
         <h1>Admin</h1>
 
-        <h2><a href="#">Users</a></h2>
-        <h2><a href="#">Orders</a></h2>
-        <h2><a href="#">Products</a></h2>
-        <h2><a href="#">Feedback</a></h2>
+        <h3><a href="#">Users</a></h3>
+        <h3><a href="#">Orders</a></h3>
+        <h3><a href="#">Products</a></h3>
+        <h3><a href="#">Feedback</a></h3>
     </div>
 </nav>
--- admin/userbase.php
+++ admin/userbase.php
@@ -19,7 +19,28 @@
         ?>
 
         <section>
+            <h1>Users</h1>
 
+            <?php
+                for( $i = 0; $i < 5; $i++ ){
+                    echo
+                    "<div class='user-card'>
+                        <img src='../images/icons/user.svg'>
+
+                        <div id='user-info'>
+                            <div id='name-email'>
+                                <h2><b>John Doe</b></h2>
+                                <h2>Johndoe@gmail.com</h2>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+
+                        <div id='remove-button'>
+                            <button>Remove User</button>
+                        </div>
+                    </div>";  
+                }     
+            ?>
         </section>
     </main>
 </body>
