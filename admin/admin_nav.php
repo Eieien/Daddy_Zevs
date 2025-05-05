fed019d5f71f45c52a5e2f76268bf6bc29e9ee1b
Ivan Paul Ruelan <greencolorapple@gmail.com>
2025-05-05 11:51:19 +0800

    admin files

---
+++ admin/admin_nav.php
@@ -0,0 +1,10 @@
+<nav id="admin-nav">
+    <div id="nav-container">
+        <h1>Admin</h1>
+
+        <h2><a href="#">Users</a></h2>
+        <h2><a href="#">Orders</a></h2>
+        <h2><a href="#">Products</a></h2>
+        <h2><a href="#">Feedback</a></h2>
+    </div>
+</nav>
---
+++ admin/userbase.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Userbase</title>
+    <link rel="stylesheet" href="../styles/constants.css?v=<?php echo time(); ?>" >
+    <link rel="stylesheet" href="../styles/header_footer.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../styles/userbase.css?v=<?php echo time(); ?>">
+</head>
+<body>
+    <?php
+        include "admin_header.php";
+
+    ?>
+    <main class="content" style="width: 75vw;">
+        <?php
+            include "admin_nav.php";
+        ?>
+
+        <section>
+
+        </section>
+    </main>
+</body>
+</html>
