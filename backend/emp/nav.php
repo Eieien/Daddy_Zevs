aa32016b9c698acc9663ccd0557f8073df5eb013
Ivan Paul Ruelan <greencolorapple@gmail.com>
2025-05-06 14:50:48 +0800

    updated styles link

--- backend/emp/nav.php
+++
@@ -1,7 +0,0 @@
-<nav>
-    <p>Admin</p>
-    <a href="userbase.php">Users</a>
-    <a href="orderlist.php">Orders</a>
-    <a href="productlist.php">Products</a>
-    <a href="feedback.php">Feedback</a>
-</nav>
