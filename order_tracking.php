828b22914e5a2460a64c6e634e9624992ee3f7ec
Eien <depedronico02@gmail.com>
2025-05-07 21:28:43 +0800

    Fixed css filepath

--- order_tracking.php
+++ order_tracking.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/constants.css">
-    <link rel="stylesheet" href="css/tracker.css">
-    <link rel="stylesheet" href="css/header_footer.css">
+    <link rel="stylesheet" href="./styles/constants.css">
+    <link rel="stylesheet" href="./styles/tracker.css">
+    <link rel="stylesheet" href="./styles/header_footer.css">
 </head>
 <body>
     <nav id="user-nav">
