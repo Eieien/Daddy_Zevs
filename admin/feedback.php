976c35988c5bef71f6f3b37d7e9e2cb409214a53
Eien <depedronico02@gmail.com>
2025-05-10 16:14:11 +0800

    finished changing admin feedback

--- admin/feedback.php
+++ admin/feedback.php
@@ -9,13 +9,6 @@
     <link rel="stylesheet" href="../styles/feedback.css?v=<?php echo time(); ?>">
 
 
-    <style>
-        .feedback-grid{
-            display: grid;
-            grid-template-columns: minmax(300px, 1fr);
-        }
-    </style>
-
 </head>
 <body>
     <?php 
@@ -29,8 +22,51 @@
 
         <section>   
             <h1>Feedback</h1>
+            <div id="feedback-list">
+                <div class="feedback-card">
+                    <div class="user-rating-container">
+                        <div class="user-profile">
+                            <img src="../images/icons/user.svg">
+                            <div>
+                                <div class="name-email">
+                                    <h2>John Doe</h2>
+                                    <span>JohnDoe@gmail.com</span>
 
-            <?php
+                                </div>
+                                <p>August 24, 2025</p>
+                            </div>
+                        </div>
+                        <div class="rating">
+                            <h3 class="total-rating">
+                                4/5
+                            </h3>
+                            <div class="star-container">
+                                <svg class="star" width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M14 19.8333L7 23.3333L8.75 16.3333L3.5 10.5L11.0833 9.91667L14 3.5L16.9167 9.91667L24.5 10.5L19.25 16.3333L21 23.3333L14 19.8333Z" stroke="#3464DD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+                                <svg class="star" width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M14 19.8333L7 23.3333L8.75 16.3333L3.5 10.5L11.0833 9.91667L14 3.5L16.9167 9.91667L24.5 10.5L19.25 16.3333L21 23.3333L14 19.8333Z" stroke="#3464DD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+                                <svg class="star" width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M14 19.8333L7 23.3333L8.75 16.3333L3.5 10.5L11.0833 9.91667L14 3.5L16.9167 9.91667L24.5 10.5L19.25 16.3333L21 23.3333L14 19.8333Z" stroke="#3464DD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+                                <svg class="star" width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M14 19.8333L7 23.3333L8.75 16.3333L3.5 10.5L11.0833 9.91667L14 3.5L16.9167 9.91667L24.5 10.5L19.25 16.3333L21 23.3333L14 19.8333Z" stroke="#3464DD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+                                <svg class="star" width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M14 19.8333L7 23.3333L8.75 16.3333L3.5 10.5L11.0833 9.91667L14 3.5L16.9167 9.91667L24.5 10.5L19.25 16.3333L21 23.3333L14 19.8333Z" stroke="#3464DD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="user-comment-container">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur vel esse quas, consequatur cum iusto nam libero aut hic corporis sapiente eum est deleniti magni consectetur quam, ratione veritatis quaerat explicabo alias, ipsa magnam laboriosam atque? Laudantium mollitia quasi totam, rerum perspiciatis tenetur reiciendis error nostrum quia itaque optio repudiandae?
+                    </div>
+                </div>
+            </div>
+
+            <!-- <?php
                 for( $i = 0; $i < 5; $i++){
                     echo
                     "<div class='user-card'>
@@ -60,7 +96,7 @@
                         </div>
                     </div>";  
                 }     
-            ?>
+            ?> -->
         </section>
 
     </main>
