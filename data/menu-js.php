16d9d1f7f2d3073b4519960036f66a437bc2bf23
Eien <depedronico02@gmail.com>
2025-05-12 14:40:35 +0800

    Added out of stock design

--- data/menu-js.php
+++ data/menu-js.php
@@ -14,29 +14,36 @@ function createProductCard(item){
     productCard.id = item.product_id;
 
     productCard.innerHTML = 
-    `<div class="favorite-icon">
-        <svg class="add-to-favorite" width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M6.2752 16.5632L16.5 27.1667L26.7248 16.5632C27.8614 15.3844 28.5 13.7857 28.5 12.1187C28.5 8.64741 25.7864 5.83334 22.4391 5.83334C20.8316 5.83334 19.29 6.49555 18.1534 7.67429L16.5 9.3889L14.8466 7.67429C13.71 6.49555 12.1684 5.83334 10.5609 5.83334C7.21356 5.83334 4.5 8.64741 4.5 12.1187C4.5 13.7857 5.13856 15.3844 6.2752 16.5632Z" stroke="#0B2027" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-    </div>
-
-    <div class="image-container">
-        <img src=${item.image}>
-    </div>
-    
-    <div class="details-container">
-        <div class="product-name">
-            ${item.product_name}
+    `<div class="out-of-stock">
+        <div class="out-of-stock-block">
+            <h1>Out Of Stock</h1>
         </div>
-        <div class="price-add">
-            <div class="price">
-                Php ${Number(item.price).toFixed(2)}
+    </div>
+    <div class="details">
+        <div class="favorite-icon">
+            <svg class="add-to-favorite" width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M6.2752 16.5632L16.5 27.1667L26.7248 16.5632C27.8614 15.3844 28.5 13.7857 28.5 12.1187C28.5 8.64741 25.7864 5.83334 22.4391 5.83334C20.8316 5.83334 19.29 6.49555 18.1534 7.67429L16.5 9.3889L14.8466 7.67429C13.71 6.49555 12.1684 5.83334 10.5609 5.83334C7.21356 5.83334 4.5 8.64741 4.5 12.1187C4.5 13.7857 5.13856 15.3844 6.2752 16.5632Z" stroke="#0B2027" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+        </div>
+
+        <div class="image-container">
+            <img src=${item.image}>
+        </div>
+        
+        <div class="details-container">
+            <div class="product-name">
+                ${item.product_name}
             </div>
-            <button class="add-to-cart">
-                <svg class="add" width="40" height="30" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M17.5 11.2917V26.7084M10.2084 19.0001H24.7917" stroke="#FBFCEC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                </svg>
-            </button>
+            <div class="price-add">
+                <div class="price">
+                    Php ${Number(item.price).toFixed(2)}
+                </div>
+                <button class="add-to-cart">
+                    <svg class="add" width="40" height="30" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M17.5 11.2917V26.7084M10.2084 19.0001H24.7917" stroke="#FBFCEC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </button>
+            </div>
         </div>
     </div>`;
 
